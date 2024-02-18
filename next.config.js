ba8092d79508558d4f7432cc0af1d49858c06e55
unknown <rawatmanoj997@gmail.com>
2024-02-18 19:34:50 +0530

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+    // ... other options you like
+});
+
 const nextConfig = {
     images: {
         domains: ['gogocdn.net', 's4.anilist.co', 'images.unsplash.com'],
     },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
